d1916b10a29061e512d14065df5383b1874187be
qi <12meridiansmedia@gmail.com>
2018-05-21 03:21:49 +0300

    Import Firebase.

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import firebase from "firebase";
+
+import reducers from "./src/reducers";
+
+const store = createStore(reducers);
 
 export default class App extends React.Component {
+  componentDidMount() {
+    const config = {
+      apiKey: "AIzaSyDlRxPcboWjajgXsyqPupIr_jMhTNfF4TQ",
+      authDomain: "manager-6e216.firebaseapp.com",
+      databaseURL: "https://manager-6e216.firebaseio.com",
+      projectId: "manager-6e216",
+      storageBucket: "manager-6e216.appspot.com",
+      messagingSenderId: "1088809690764"
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Text>Hello!</Text>
+        </View>
+      </Provider>
     );
   }
 }
@@ -14,8 +36,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
