836377e95f5eaced70172883fb5ad8f0cda320fe
qi <12meridiansmedia@gmail.com>
2018-05-24 01:50:23 +0300

    Get list of employees.

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import { connect } from "react-redux";
+
+import { employeesFetch } from "../actions";
 
 class EmployeeList extends Component {
+  componentDidMount() {
+    this.props.employeesFetch();
+  }
+
   render() {
     return (
       <View>
@@ -16,4 +23,4 @@ class EmployeeList extends Component {
   }
 }
 
-export default EmployeeList;
+export default connect(null, { employeesFetch })(EmployeeList);
